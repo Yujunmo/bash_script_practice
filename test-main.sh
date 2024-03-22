ef23a10d2ba4cbff709b6fbb182f439cace61e58
Yujunmo <45279586+Yujunmo@users.noreply.github.com>
2024-03-22 13:52:22 +0900

    Add files via upload

---
+++ test-main.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+source include/user.sh
+source include/firewalld.sh
+
+case $1 in
+	add)
+		case $2 in
+			service)
+				echo "add firewalld service"
+				SetFirewall $3
+				;;
+		esac
+		;;
+	remove)
+		case $2 in
+			service)
+				echo "remove firewalld service"
+				RemoveFirewall $3
+				;;
+		esac
+		;;
+	
+	list)
+		case $2 in
+			service)
+				echo "list firewalld service"
+				ListFirewall $3
+				;;
+		esac
+		;;
+	
+	create)
+		case $2 in
+			user)
+				echo "this is create"
+				CreateUser $3 $4
+				;;
+		esac
+		;;
+	delete)
+		case $2 in 
+			user)
+			       echo "this is delete"
+			       DeleteUser $3
+			       ;;
+	        esac
+		;;
+	
+esac
